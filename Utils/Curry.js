9faee966957e588b5dc00855a30014dd5a1cc955
Antony Britto <antony.britto@flipkart.com>
2016-01-13 11:27:06 +0530

    Adding native method check for curry

--- Utils/Curry.js
+++ Utils/Curry.js
@@ -1,9 +1,6 @@
-const curry = (func) => {
+Function.prototype.curry = Function.prototype.curry || function(func) {
     args = Array.prototype.slice.call(arguments);
     return function() {
-      return func.apply(this, args.concat(
-        Array.prototype.slice.call(arguments)));
+      return func.apply(this, args.concat(Array.prototype.slice.call(arguments)));
     };
 };
-
-export default curry;
